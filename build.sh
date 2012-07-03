6106d1ef0913bd18d698d8978a9a7930a34d9880
Michael Rumpf <michael@rumpfonline.de>
2012-07-03 20:40:58 +0200

    Fixed build script

--- build.sh
+++ build.sh
@@ -1,36 +1,49 @@
 #!/bin/bash
 
-DIR=.
+DIR=$(pwd)
 
-mkdir install
-mkdir gupnp
+rm -rf ${DIR}/install
+rm -rf ${DIR}/gupnp
+
+mkdir ${DIR}/install
+mkdir ${DIR}/gupnp
 
 cd gupnp
 
 git clone http://git.gnome.org/browse/gssdp
 cd gssdp
-./autogen.sh --prefix=../../install --exec-prefix=../../install
+./autogen.sh --prefix=${DIR}/install --exec-prefix=${DIR}/install
 make && make install
 cd ..
 
 
 git clone http://git.gnome.org/browse/gupnp
 cd gupnp
-PKG_CONFIG_PATH=../../install/lib/pkgconfig ./autogen.sh --prefix=../../install --exec-prefix=../../install
+PKG_CONFIG_PATH=${DIR}/install/lib/pkgconfig ./autogen.sh --prefix=${DIR}/install --exec-prefix=${DIR}/install
+make && make install
+cd ..
+
+
+git clone http://git.gnome.org/browse/gupnp-av
+cd gupnp-av
+PKG_CONFIG_PATH=${DIR}/install/lib/pkgconfig ./autogen.sh --prefix=${DIR}/install --exec-prefix=${DIR}/install
+make && make install
 cd ..
 
 
 git clone http://git.gnome.org/browse/gupnp-tools
 cd gupnp-tools
-PKG_CONFIG_PATH=../../install/lib/pkgconfig ./autogen.sh --prefix=../../install --exec-prefix=../../install
+PKG_CONFIG_PATH=${DIR}/install/lib/pkgconfig ./autogen.sh --prefix=${DIR}/install --exec-prefix=${DIR}/install
+make && make install
 cd ..
 
 
 cd ..
 
-export PATH=./install/bin:./gupnp/gupnp/examples:$PATH
-export LD_LIBRARY_PATH=./install/lib:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=./install/lib/pkgconfig:$PKG_CONFIG_PATH
+export PATH=${DIR}/install/bin:${DIR}/gupnp/gupnp/examples:${DIR}/gupnp/gupnp/tools:${DIR}/gupnp/gssdp/tools:$PATH
+export LD_LIBRARY_PATH=${DIR}/install/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=${DIR}/install/lib/pkgconfig:$PKG_CONFIG_PATH
 
-PKG_CONFIG_PATH=install/lib/pkgconfig ./autogen.sh --prefix=install --exec-prefix=install
+PKG_CONFIG_PATH=${DIR}/install/lib/pkgconfig ./autogen.sh --prefix=${DIR}/install --exec-prefix=${DIR}/install
+make && make install
 
